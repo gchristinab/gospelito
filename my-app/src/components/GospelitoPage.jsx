448df5740174aaba7997ee2ad99b07e3604e2d99
Gigi <63467779+gcbrusco@users.noreply.github.com>
2025-10-15 22:13:46 -0400

    Update

--- my-app/src/components/GospelitoPage.jsx
+++ my-app/src/components/GospelitoPage.jsx
@@ -6,26 +6,28 @@ const GospelitoPage = () => {
   return (
     <div className="container">
       <div className="content">
-        <h1 className="title">GOSPELITO</h1>
-        <p className="subtitle">Sunday Gospel Coloring Book for Children</p>
+        <h1 className="title">Sunday Gospel Coloring Book</h1>
+        <p className="subtitle">2026 Edition</p>
         <a
-          href="https://www.amazon.com/Gospelito-Sunday-Gospel-Coloring-Children/dp/B0DYD19N6G/ref=sr_1_1?dib=eyJ2IjoiMSJ9.r8IW10DVEzZVA2mSj7cVwQ.XpDi2p7-jYAxkfm6-dVou75NexiMNwAlxQexX2GGSAE&dib_tag=se&qid=1741193860&refinements=p_27%3AGospelito+Books&s=books&sr=1-1&text=Gospelito+Books"
+          href="https://www.amazon.com/author/gospelito-books"
           target="_blank"
           rel="noopener noreferrer"
         >
           <img
             className="book-image clickable"
-            src="https://m.media-amazon.com/images/I/71oplcQkcEL._SY522_.jpg"
+            src="/professional_cover_3.png"
             alt="Gospelito Coloring Book"
           />
         </a>
         <p className="description">
           A fun and engaging way for children to connect with the Sunday Gospel
-          through coloring! Perfect for families and religious educators.
+          through coloring!
+          <br></br>
+          Perfect for families and religious educators.
         </p>
         <a
           className="buy-button"
-          href="https://www.amazon.com/Gospelito-Sunday-Gospel-Coloring-Children/dp/B0DYD19N6G/ref=sr_1_1?dib=eyJ2IjoiMSJ9.r8IW10DVEzZVA2mSj7cVwQ.XpDi2p7-jYAxkfm6-dVou75NexiMNwAlxQexX2GGSAE&dib_tag=se&qid=1741193860&refinements=p_27%3AGospelito+Books&s=books&sr=1-1&text=Gospelito+Books"
+          href="https://www.amazon.com/author/gospelito-books"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -37,17 +39,17 @@ const GospelitoPage = () => {
       <footer className="footer">
         <div className="social-icons">
           <a
-            href="https://www.instagram.com/gospelitobooks/"
+            href="https://www.instagram.com/gospelcoloringbook/"
             target="_blank"
             rel="noopener noreferrer"
           >
             <FaInstagram className="icon" />
           </a>
-          <a href="mailto:admin@gospelito.com">
+          <a href="mailto:gospelitobooks@gmail.com">
             <FaEnvelope className="icon" />
           </a>
         </div>
-        <p className="rights">© 2025 Gospelito. All rights reserved.</p>
+        <p className="rights">© 2025 Gospelito Books. All rights reserved.</p>
       </footer>
     </div>
   );
